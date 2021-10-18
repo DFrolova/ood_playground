1afb760276b3e713e5c3cde431fe5057ca1ba328
BorisShirokikh <bg.dustpelt@gmail.com>
2021-10-18 18:37:44 +0300

    FEAT: add lits and luna datasets

--- ood/dataset/lits.py
+++ ood/dataset/lits.py
@@ -1,1 +1,21 @@
-# TODO: add LiTS dataset
+import numpy as np
+
+from dpipe.dataset.wrappers import Proxy
+from neurodata.lits import LiTS as LiTSNeurodata
+
+
+class LiTS(Proxy):
+    def __init__(self):
+        super().__init__(LiTSNeurodata())
+
+    def load_image(self, i):
+        return np.float32(self.image(i))
+
+    def load_segm(self, i):
+        if 'test' in i:
+            raise IndexError('Trying to load mask for the test image')
+        else:
+            return np.float32(self.segmentation(i))
+
+    def load_spacing(self, i):
+        return np.float32(self.spacing(i))
--- ood/dataset/luna.py
+++ ood/dataset/luna.py
@@ -1,1 +1,20 @@
-# TODO: add LUNA16 dataset
+import numpy as np
+
+from dpipe.dataset.wrappers import Proxy
+from neurodata.lidc import LIDC
+
+
+class LUNA16(Proxy):
+    def __init__(self):
+        super().__init__(LIDC())
+
+    def load_image(self, i):
+        return np.float32(self.image(i))
+
+    def load_segm(self, i):
+        return np.float32(self.cancer(i))
+
+    def load_spacing(self, i):
+        diffs, counts = np.unique(np.round(np.diff(self.slice_locations(i)), decimals=5), return_counts=True)
+        pixel_spacing = self.pixel_spacing(i)
+        return np.float32([pixel_spacing[0], pixel_spacing[1], diffs[np.argsort(counts)[-1]]])
