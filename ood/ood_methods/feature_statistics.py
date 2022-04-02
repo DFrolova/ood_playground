4f296d05a69caf56171219e5d68a981ce875415a
Daria Frolova <d.frolova@ira-labs.com>
2022-04-02 15:01:42 +0300

    refactor feature_statistics and svd

--- ood/ood_methods/feature_statistics.py
+++ ood/ood_methods/feature_statistics.py
@@ -12,23 +12,3 @@ def get_mean_std(feature):
     means = np.mean(feature, axis=SPATIAL_DIMS)
     stds = np.std(feature, axis=SPATIAL_DIMS)
     return np.concatenate((means, stds))
-    
-    
-def get_ood_scores_from_mean_std(test_ids, train_predictions_path, spectrum_folder, results_path, exist_ok=False):
-    results = defaultdict(dict)
-    
-    train_ids = load(os.path.join(train_predictions_path, 'train_ids.json'))
-    train_matrix = np.stack([load(os.path.join(train_predictions_path, spectrum_folder, f'{uid}.npy')) 
-                             for uid in train_ids])
-    
-    for uid in test_ids:
-        spectrum = load(os.path.join(spectrum_folder, f'{uid}.npy'))
-        distances = np.linalg.norm(train_matrix - spectrum, axis=1)
-        results['min_distance'][uid] = min(distances)
-        results['5_percentile'][uid] = np.percentile(distances, 5)
-        results['mean_distance'][uid] = np.mean(distances)
-        results['distance_from_center'][uid] = np.linalg.norm((train_matrix.mean(axis=0) - spectrum))
-    
-    os.makedirs(results_path, exist_ok=exist_ok)
-    for metric_name, result in results.items():
-        save_json(result, os.path.join(results_path, metric_name + '.json'), indent=0)
--- ood/ood_methods/svd.py
+++ ood/ood_methods/svd.py
@@ -21,6 +21,16 @@ def get_ood_scores_from_spectrum(test_ids, train_predictions_path, spectrum_fold
     train_matrix = np.stack([load(os.path.join(train_predictions_path, spectrum_folder, f'{uid}.npy')) 
                              for uid in train_ids])
     
+    # scaled matrix
+    max_vals = train_matrix.max(axis=0)
+    min_vals = train_matrix.min(axis=0)
+    train_matrix_scaled = (train_matrix - min_vals) / (max_vals - min_vals)
+    
+    # normalized matrix
+    mean_vals = train_matrix.mean(axis=0)
+    std_vals = train_matrix.std(axis=0)
+    train_matrix_norm = (train_matrix - mean_vals) / std_vals
+    
     for uid in test_ids:
         spectrum = load(os.path.join(spectrum_folder, f'{uid}.npy'))
         distances = np.linalg.norm(train_matrix - spectrum, axis=1)
@@ -28,6 +38,22 @@ def get_ood_scores_from_spectrum(test_ids, train_predictions_path, spectrum_fold
         results['5_percentile'][uid] = np.percentile(distances, 5)
         results['mean_distance'][uid] = np.mean(distances)
         results['distance_from_center'][uid] = np.linalg.norm((train_matrix.mean(axis=0) - spectrum))
+        
+        # scaled
+        spectrum_scaled = (spectrum - mean_vals) / std_vals
+        distances_scaled = np.linalg.norm(train_matrix_scaled - spectrum_scaled, axis=1)
+        results['min_distance_scale'][uid] = min(distances_scaled)
+        results['5_percentile_scale'][uid] = np.percentile(distances_scaled, 5)
+        results['mean_distance_scale'][uid] = np.mean(distances_scaled)
+        results['distance_from_center_scale'][uid] = np.linalg.norm((train_matrix_scaled.mean(axis=0) - spectrum_scaled))
+        
+        # normalized
+        spectrum_norm = (spectrum - mean_vals) / std_vals
+        distances_norm = np.linalg.norm(train_matrix_norm - spectrum_norm, axis=1)
+        results['min_distance_norm'][uid] = min(distances_norm)
+        results['5_percentile_norm'][uid] = np.percentile(distances_norm, 5)
+        results['mean_distance_norm'][uid] = np.mean(distances_norm)
+        results['distance_from_center_norm'][uid] = np.linalg.norm((train_matrix_norm.mean(axis=0) - spectrum_norm))
     
     os.makedirs(results_path, exist_ok=exist_ok)
     for metric_name, result in results.items():
