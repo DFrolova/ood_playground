b3b447cb28e935b1313f3da3392dffd1a3328c3c
Daria Frolova <d.frolova@ira-labs.com>
2022-06-20 08:23:35 +0300

    filter lits test ids

--- ood/dataset/lits.py
+++ ood/dataset/lits.py
@@ -7,6 +7,7 @@ from neurodata.lits import LiTS as LiTSNeurodata
 class LiTS(Proxy):
     def __init__(self):
         super().__init__(LiTSNeurodata())
+        self.ids = [uid for uid in self.ids if not uid.startswith('lits-test-')]
 
     def load_image(self, i):
         return np.float32(self.image(i))
