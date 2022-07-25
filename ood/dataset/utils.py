2c20cf3847eed7885a78e54922753144bfdd0433
Daria Frolova <d.frolova@ira-labs.com>
2022-07-25 10:05:14 +0300

    updated config files for lidc

--- ood/dataset/utils.py
+++ ood/dataset/utils.py
@@ -2,7 +2,6 @@ from typing import Union
 
 import numpy as np
 from connectome import Transform
-from skimage.measure import label
 
 from dpipe.im.shape_ops import zoom, crop_to_box, pad
 
@@ -37,29 +36,13 @@ class Zoom(Transform):
     def _scale_factor(voxel_spacing, _new_spacing):
         if not isinstance(_new_spacing, (tuple, list, np.ndarray)):
             _new_spacing = np.broadcast_to(_new_spacing, 3)
-        print('voxel spacing', voxel_spacing)
-        print(np.nan_to_num(np.float32(voxel_spacing) / np.float32(_new_spacing), nan=1))
         return np.nan_to_num(np.float32(voxel_spacing) / np.float32(_new_spacing), nan=1)
 
     def image(image, _scale_factor, _order):
-        print('image shape', image.shape)
         return np.array(zoom(image.astype(np.float32), _scale_factor, order=_order))
 
     def mask(mask, _scale_factor):
         return np.array(zoom(mask.astype(np.float32), _scale_factor) > 0.5, dtype=mask.dtype)
-
-    def new_voxel_spacing(_new_spacing):
-        return _new_spacing
-
-    def old_voxel_spacing(voxel_spacing):
-        return voxel_spacing
-
-
-class FixVoxelSpacing(Transform):
-    __inherit__ = True
-
-    def voxel_spacing(new_voxel_spacing):
-        return new_voxel_spacing
 
 
 class ZoomImage(Transform):
@@ -80,5 +63,5 @@ class VoxelSpacing(Transform):
     __inherit__ = True
 
     def voxel_spacing(pixel_spacing, slice_locations):
-        diffs, counts = np.unique(np.round(np.diff(slice_locations), decimals=5), return_counts=True)
-        return np.float32([pixel_spacing[0], pixel_spacing[1], abs(-diffs[np.argsort(counts)[-1]])])
+        slice_spacing = abs(np.round(np.median(np.diff(slice_locations)), decimals=5))
+        return np.float32([pixel_spacing[0], pixel_spacing[1], slice_spacing])
