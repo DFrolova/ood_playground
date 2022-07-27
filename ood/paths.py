a36a8557a2cda2a028f0415bb95576aba88f0e34
Daria Frolova <d.frolova@ira-labs.com>
2022-07-27 11:55:31 +0300

    more configs for lidc setup

--- ood/paths.py
+++ ood/paths.py
@@ -5,7 +5,11 @@ from .utils import choose_root
 LIDC_DATA_PATH = None
 LITS_DATA_PATH = None
 MEDSEG9_DATA_PATH = None
+MIDRC_DATA_PATH = None
 CANCER500_DATA_PATH = None
+CT_ICH_DATA_PATH = None
+
+LIDC_ARTIFACTS_DATA_PATH = '/shared/experiments/ood_playground/artifacts_lidc'
 
 ENSEMBLE_MODELS_PATH_LIDC = choose_root(
     '/shared/experiments/ood_playground/lidc/lidc/',
